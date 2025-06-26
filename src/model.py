5e15999ae3a6d303c9e5ff633def03363fd9cd8a
Theo CREPIN <theo.crepin@epitech.eu>
2025-06-26 21:18:48 +0200

    fix: changed the way to get classes name, to prevent loading the whole dataset for this.

--- src/model.py
+++ src/model.py
@@ -8,36 +8,36 @@ os.environ['TF_ENABLE_ONEDNN_OPTS'] = '0'
 os.environ['TF_FORCE_GPU_ALLOW_GROWTH'] = 'true'
 import tensorflow as tf
 from PIL import Image
-import pickle
 
 class ImageClassifier:
-    def __init__(self, model_path="model_classification.h5", dataset_path="dataset.pkl"):
+    def __init__(self, model_path="model_classification.h5"):
         self.model_path = model_path
-        self.dataset_path = dataset_path
         self.model = None
-        self.class_names = None
+        self.class_names = [
+            "butterfly",
+            "cat", 
+            "chicken",
+            "cow",
+            "dog",
+            "elephant",
+            "horse",
+            "sheep",
+            "spider",
+            "squirrel"
+        ]
         self.image_size = (128, 128)
         self.load_model()
-        self.load_class_names()
     
     def load_model(self):
         if not os.path.exists(self.model_path):
             raise FileNotFoundError(f"Model not found: {self.model_path}")
         self.model = tf.keras.models.load_model(self.model_path)
     
-    def load_class_names(self):
-        if not os.path.exists(self.dataset_path):
-            raise FileNotFoundError(f"Dataset not found: {self.dataset_path}")
-        with open(self.dataset_path, 'rb') as f:
-            dataset = pickle.load(f)
-        self.class_names = dataset['class_names']
-    
     def preprocess_image(self, image_path):
         try:
             with Image.open(image_path) as img:
                 if img.mode != 'RGB':
                     img = img.convert('RGB')
-                
                 img_resized = img.resize(self.image_size, Image.Resampling.LANCZOS)
                 img_array = np.array(img_resized)
                 img_array = img_array.astype('float32') / 255.0
@@ -53,12 +53,12 @@ class ImageClassifier:
         predicted_class_idx = np.argmax(probabilities)
         predicted_class = self.class_names[predicted_class_idx]
         confidence = probabilities[predicted_class_idx]
-
+        
         results = {
             'predicted_class': predicted_class,
             'confidence': float(confidence),
             'probabilities': {
-                self.class_names[i]: float(probabilities[i]) 
+                self.class_names[i]: float(probabilities[i])
                 for i in range(len(self.class_names))
             }
         }
